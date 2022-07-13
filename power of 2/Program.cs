15dfb79d32fb1af6a9e0fbdf747a98dfbf7f88eb
mani1 <mani1@NANI143>
2022-07-13 11:10:27 +0530

    power of 2

---
+++ power of 2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class powerof2
+{
+    static bool PowerOfTwo(int n)
+    {
+        return (int)(Math.Ceiling((Math.Log(n) / Math.Log(2))))
+            == (int)(Math.Floor(Math.Log(n) / Math.Log(2))));
+
+    }
+    public static void main()
+    {
+        if (PowerOfTwo(31))
+            Console.WriteLine("yes");
+        else
+            Console.WriteLine("No");
+        if (PowerOfTwo(64))
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("No");
+
+
+    }
+
+}
